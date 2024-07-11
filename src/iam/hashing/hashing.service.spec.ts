4049a55db67b6def26fa997bab642d8238c5b495
Aman <dalmi.aman@gmail.com>
2024-07-11 14:42:25 -0400

    chore: Implement BcryptService for password hashing

---
+++ src/iam/hashing/hashing.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HashingService } from './hashing.service';
+
+describe('HashingService', () => {
+  let service: HashingService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: HashingService,
+          useFactory: () => ({
+            hash: jest.fn(),
+            compare: jest.fn(),
+          }),
+        },
+      ],
+    }).compile();
+
+    service = module.get<HashingService>(HashingService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
