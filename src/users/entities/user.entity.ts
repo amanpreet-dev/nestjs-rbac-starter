1e56e1056c2821c8b6f4f5780b578ccbcb805459
Aman <dalmi.aman@gmail.com>
2024-07-11 14:29:15 -0400

    feat: Add User entity and module with Mongoose integration

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  role: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
